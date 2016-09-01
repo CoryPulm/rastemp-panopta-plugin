84ba87bf41dd1cdbed854601e352894f22ffd9b6
Cory Pulm <cory.pulm@gmail.com>
2016-09-01 16:11:39 -0500

    only does cpu and gpu for now. I think thats all you can really collect anyway

---
+++ raspi_temps.py
@@ -0,0 +1,53 @@
+import agent_util
+import os
+
+class RPTemperaturePlugin(agent_util.Plugin):
+    textkey = "raspi_temp"
+    label = "Raspi Temperature"
+
+    @classmethod
+    def get_metadata(self, config):
+        status = agent_util.SUPPORTED
+        msg = None
+
+
+        # Core Linux
+        if not os.path.isfile("/sys/class/thermal/thermal_zone0/temp"):
+            msg = "Either not a Raspi or thermal zone file does not exist!"
+            status = agent_util.UNSUPPORTED
+            return {}
+
+        metadata = {
+            "cpu_temp": {
+                "label": "CPU Temperature",
+                "options": None,
+                "status": status,
+                "error_message": msg,
+                "unit": "C"
+            },
+            "gpu_temp": {
+                "label": "GPU Temperature",
+                "options": None,
+                "status": status,
+                "error_message": msg,
+                "unit": "C"
+            },
+        }
+
+        return metadata
+
+    def check(self, textkey, data, config={}):
+        if textkey == 'cpu_temp':
+            ret, raw_temp = agent_util.execute_command("cat /sys/class/thermal/thermal_zone0/temp")
+            t1 = float(raw_temp) / 1000
+            t2 = float(raw_temp) / 100
+            cpu_temp = float(t2 % t1)
+            return float("%.1f" % cpu_temp)
+
+        if textkey == 'gpu_temp':
+            ret, raw_temp = agent_util.execute_command("/opt/vc/bin/vcgencmd measure_temp")
+            gpu_temp = raw_temp.replace("temp=", "")
+            gpu_temp = gpu_temp.replace("'C", "")
+            return float(gpu_temp)
+
+        return 1337
